d48b442c5f88c1ade1997a568909dff4d69aae81
Wayne <wayne851210@gmail.com>
2020-05-18 16:37:10 +0800

    Initial commit

---
+++ ScreenRecordDemoTests/ScreenRecordDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ScreenRecordDemoTests.swift
+//  ScreenRecordDemoTests
+//
+//  Created by 江又靖 on 2020/5/17.
+//  Copyright © 2020 YouJing. All rights reserved.
+//
+
+import XCTest
+@testable import ScreenRecordDemo
+
+class ScreenRecordDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
